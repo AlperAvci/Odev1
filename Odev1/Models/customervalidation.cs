df4b7cd302a3586bb47bb65207aaa2f294c2b256
Alper Avcı <avcialper954@gmail.com>
2022-04-07 13:13:06 +0300

    Add project files.

---
+++ Odev1/Models/customervalidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Odev1.Models
+{
+    [MetadataType(typeof(customerMetaData))]
+    public partial class customer
+    {
+        public class customerMetaData
+        { 
+
+        [DisplayName("CustomerName")]
+        public string custName { get; set; }
+
+            [DisplayName("Adres")]
+            public string adres { get; set; }
+
+            [DisplayName("Mobile")]
+            public Nullable<int> mobile { get; set; }
+        }
+    }
+}
